d7e51b22ce9fc933c872c1f9480301e8817ae6a3
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-01-04 12:09:06 +0100

    basic setup

---
+++ kirkbot/cli.py
@@ -0,0 +1,11 @@
+import click
+
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+def hello(count):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo(f"Hello world!")
+
+if __name__ == '__main__':
+    hello()
---
+++ tests/test_noop.py
@@ -0,0 +1,4 @@
+import pytest
+
+def test_noop():
+    assert True
